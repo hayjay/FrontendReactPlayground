84afefab5002a63db4d8a4002beab4a97e3e34ea
Nurudeen Ajayi <ydeveloper15@gmail.com>
2020-07-12 23:08:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Ninjas from './Ninjas';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First React App!</h1>
+      <p>Welcome :)</p>
+      <Ninjas name="Nurudeen" age="34" belt="Brown"/>
+      <Ninjas name="Dhikrullah" age="89" belt="Yellow"/>
+
     </div>
   );
 }
