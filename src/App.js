b21b1126cea50f934fc4f72beae4314e9291678b
Nurudeen Ajayi <ydeveloper15@gmail.com>
2020-07-13 00:48:26 +0100

    Passed Data from parent component to child component using props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Ninjas from './Ninjas';
 function App() {
+  const state = {
+    ninjas : [
+      { "name" : "Nurudeen Ajayi", "age" : 23, id : 1, "belt" : "green" },
+      { "name" : "Ishola Kazeem", "age" : 46, id : 3, "belt" : "yellow" }
+    ] 
+  }
   return (
     <div className="App">
       <h1>My First React App!</h1>
       <p>Welcome :)</p>
-      <Ninjas name="Nurudeen" age="34" belt="Brown"/>
-      <Ninjas name="Dhikrullah" age="89" belt="Yellow"/>
+      <Ninjas ninjas={state.ninjas}/>
 
     </div>
   );
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -5,12 +5,22 @@ class Ninjas extends Component {
         // console.log(this.props) //by default when a parent component passes a property to a child component, 
         //the child component would see the props thats been pass and can access all the props using this.props.propname
         //Destrucing.. destructure the name, age and belt properties from this.props.name to name and this.props.age to age                
-        const { name, age, belt } = this.props;         
+        const { ninjas } = this.props;
+        //iterate over our ninjas list and display the list in jsX formats to the user / parent component who needs this component
+        const ninjasList = ninjas.map((each_record) => {
+            return (
+                <div className="ninja" key={each_record.id}>
+                    <div>Name : { each_record.name }</div>
+                    <div>Age : { each_record.age }</div>
+                    <div>Belt : { each_record.belt }</div>
+                </div>
+            )    
+        })
+
         return (
-            <div className="ninja">
-                <div>Name : { name }</div>
-                <div>Age : { age }</div>
-                <div>Belt : { belt }</div>
+            <div className="ninja-list">
+                {/* In this div, Display our ninja list jsX variable which holds the ninjas data */}
+                { ninjasList } 
             </div>
         )
     }
